68118ad6b0047c5b9865b42d995d94ee9cbfeb71
Shota Miyazaki <myzk.study@gmail.com>
2025-07-24 03:19:21 +0900

    管理者のroute設定admin/owners/～。 [memo] ログインしてる場合のみ表示できるようにするためにmiddleware('auth:admin')で設定してる

--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,10 @@ use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 Route::get('/', function () {
     return view('admin.welcome');
 });
+
+// ownersのRouteを設定、middleware('auth:admin')で管理者でログインしてる場合のみ表示できるようにする
+Route::resouce('owners', OwnersController::class)
+->middleware('auth:admin');
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
