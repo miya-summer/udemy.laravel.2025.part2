528af8e090f7b2b770c36630ae17c3ef454b2175
Shota Miyazaki <myzk.study@gmail.com>
2025-07-25 01:45:06 +0900

    シーダー（ダミーデータの作成） [実行コマンド] php artisan make:seeder AdminSeeder php artisan make:seeder OwnerSeeder 上記コマンドでSeederファイルの作成 php artisan db:seed でseederの実行をするが、今回はメールアドレスの重複があったので、 php artisan migrate:refresh --seed こちらのコマンドでテーブルごと作り直す方法に実行

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;      // 追加
+use Illuminate\Support\Facades\Hash;    // PWをhash化するため追加
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => Hash::make('password123'),
+            'created_at' => '2025/07/25 01:35:00'
+        ]);
+    }
+}
---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;      // 追加
+use Illuminate\Support\Facades\Hash;    // PWをhash化するため追加
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('owners')->insert([
+            [
+                'name' => 'test1',
+                'email' => 'test1@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2025/07/25 01:35:00'
+            ],
+            [
+                'name' => 'test2',
+                'email' => 'test2@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2025/07/25 01:35:00'
+            ],
+            [
+                'name' => 'test3',
+                'email' => 'test3@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2025/07/25 01:35:00'
+            ],
+        ]);
+    }
+}
