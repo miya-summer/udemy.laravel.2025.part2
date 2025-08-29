492dbc918721ee222f6306bfbcba49f200f7e016
Shota Miyazaki <myzk.study@gmail.com>
2025-08-30 01:08:51 +0900

    Product model初期設定

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
+use App\Models\SecondaryCategory;
+use App\Models\Image;
 
 class Product extends Model
 {
@@ -13,5 +15,13 @@ class Product extends Model
     public function shop()
     {
         return $this->belongsTo(Shop::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(SecondaryCategory::class, 'secondary_category_id');
+    }
+
+    public function imageFirst(){
+        return $this->belongsTo(Image::class, 'image1', 'id');
     }
 }
