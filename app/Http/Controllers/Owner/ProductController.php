b5d627cdebfbaf7afdde717c2c792d6d67b75adf
Shota Miyazaki <myzk.study@gmail.com>
2025-09-02 10:56:00 +0900

    定数化対応 app/Constants/Common.phpを追加して対応する。

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Owner;
 
+use App\Constants\Common;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Image;
@@ -205,11 +206,11 @@ class ProductController extends Controller
                     $product->save();
 
                     // 在庫変更処理
-                    if($request->type === '1') {
+                    if($request->type === \Constant::PRODUCT_LIST['add']) {
                         $newQuantity = $request->quantity;
                     }
 
-                    if($request->type === '2') {
+                    if($request->type === \Constant::PRODUCT_LIST['reduce']) {
                         $newQuantity = $request->quantity * -1;
                     }
 
