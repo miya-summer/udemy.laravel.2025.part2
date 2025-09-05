feb011dd4da440ddd1b7b5756fbb16b7de2e9d2f
Shota Miyazaki <myzk.study@gmail.com>
2025-09-05 14:54:29 +0900

    商品一覧ページを開いたときは、表示件数をデフォルト20件にする。

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -62,7 +62,7 @@ class ItemController extends Controller
 
         $products = Product::availableItems()
             ->sortOrder($request->sort)
-            ->paginate($request->pagination);
+            ->paginate($request->pagination ?? '20');
 //        dd($stocks, $products);
 
 //        $products = Product::all();
